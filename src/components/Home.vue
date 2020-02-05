5fa042eff765498941a89596f60989defbe1996f
dicka_ismaji <dicka_ismaji@quick.com>
2020-02-05 15:19:49 +0700

    first

---
+++ src/components/Home.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="container">
+        <header>
+            Daftar User Github
+        </header>
+        <div class="list">
+            <ul v-for="user in users" :key="user.username" style="list-style: none">
+                <li>
+                    <router-link :to="{ name: 'User' , params : {id : user.username}}">
+                        {{ user.username }} - {{ user.name }}
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+module.exports =  {
+    data() {
+        return {
+            users: []
+        }
+    },
+    created() {
+        this.getAllUser()
+    },
+    methods: {
+        getAllUser() {
+
+            this.users = [
+                {
+                    username: 'dicka88',
+                    name: 'Dicka Ismaji'
+                },
+                {
+                    username: 'thembig',
+                    name: 'Malik Fajar'
+                },
+                {
+                    username: 'jovianreynaldo',
+                    name: 'Alphonsus Jovian Joy Reynaldo'
+                },
+                {
+                    username: 'guntoroyk',
+                    name: 'Guntoro Yudhi Kusumo'
+                },
+                {
+                    username: 'daffastefian',
+                    name: 'Daffa Stefian Abyansyah'
+                },
+                {
+                    username: 'aldipradana',
+                    name: 'Aldi Pradana'
+                },
+                {
+                    username: 'ezralazuardy',
+                    name: 'Ezra Lazuardy Wijaya',
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    header {
+        display: block;
+        width: 100%;
+        background-color: green;
+        color: white;
+    }
+    .list {
+        margin-left: -50px;
+    }
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <div class="user">
+            <h2>Data github dengan nama: {{ user.name }}</h2>
+            <ul>
+                <li>Username : {{ user.login }}</li>
+                <li>Username : {{ user.email }}</li>
+            </ul>
+            <router-link to="/">Back</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+const axios = require('axios')
+
+module.exports = {
+    data() {
+        return {
+            user: {}
+        }
+    },
+    created() {
+        this.getUser()
+    },
+    methods: {
+        getUser() {
+            let vm = this
+            let username = this.$route.params.id
+            let endpoint = 'https://api.github.com/users/' + username
+            axios.get(endpoint)
+                .then(res => {
+                    vm.user = res.data
+                })
+                .catch(res => {
+                    console.log(err)
+                })
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    ul, li {
+        list-style: none;
+    }
+</style>
